3596b6d3f9f01151708716f4bd3504c383aabebd
chandra sekhar <cssahu3988@gmail.com>
2023-07-01 15:38:35 +0530

    c-5

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-//const dgram = require('node:dgram');
-const express = require('express');
-const app = express();
-app.use(express.json());
+const dgram = require('node:dgram');
+// const express = require('express');
+// const app = express();
+// app.use(express.json());
 
 
-app.get('/', (req,res)=>{
-    res.status(200).send({"msj":"hello from server"});
-})
+// app.get('/', (req,res)=>{
+    // res.status(200).send({"msj":"hello from server"});
+// })
 
-app.listen(3000,()=>{
-    console.log('listening on port 3000');
-})
+// app.listen(3000,()=>{
+    // console.log('listening on port 3000');
+// })
 
-// const server = dgram.createSocket('udp4');
+const server = dgram.createSocket('udp4');
 
-// server.on('error', (err) => {
-//   console.error(`server error:\n${err.stack}`);
-//   server.close();
-// });
+server.on('error', (err) => {
+  console.error(`server error:\n${err.stack}`);
+  server.close();
+});
 
-// server.on('message', (msg, rinfo) => {
-//   console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-//   console.log(Uint8Array.from(Buffer.from(msg)));
-//   server.send("Hello", rinfo.port, rinfo.address);
-// });
+server.on('message', (msg, rinfo) => {
+  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+  console.log(Uint8Array.from(Buffer.from(msg)));
+  server.send("Hello", rinfo.port, rinfo.address);
+});
 
-// server.on('listening', () => {
-//   const address = server.address();
-//   console.log(`server listening ${address.address}:${address.port}`);
-// });
+server.on('listening', () => {
+  const address = server.address();
+  console.log(`server listening ${address.address}:${address.port}`);
+});
 
-// server.bind(3000);
+server.bind(3000);
